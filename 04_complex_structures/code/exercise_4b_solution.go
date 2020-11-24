67db60f22db3ef225fd31b7f6c537e6b0b50ffdf
Katarzyna <kolnykatarzyna@gmail.com>
2020-11-24 18:58:57 +0000

    Feat: solution 4 MAP SLICE

--- 04_complex_structures/code/exercise_4b_solution.go
+++ 04_complex_structures/code/exercise_4b_solution.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func average(args ...float64) float64 {
-	total := 0.0
-	for _, args := range args  {
-    total += args
-	}
-	return total/float64(len(args))
-}
+// func average(args ...float64) float64 {
+// 	total := 0.0
+// 	for _, args := range args  {
+//     total += args
+// 	}
+// 	return total/float64(len(args))
+// }
 
-func main() {
-	fmt.Println(average(8,7,9))
-}
+// func main() {
+// 	fmt.Println(average(8,7,9))
+// }
--- 04_complex_structures/code/exercise_4c_solution.go
+++ 04_complex_structures/code/exercise_4c_solution.go
@@ -1,17 +1,44 @@
 package main
-
-// import (
-// 	"fmt"
-// )
-
-// func average(numbers ...float64) float64 {
-// 	total := 0.0
-// 	for _, number := range numbers {
-// 		total += number
+import "fmt"
+// func average(scores [5]float64) float64 {
+// 		total := 0.0
+//   for _, score := range scores {
+//     total += score
 // 	}
-// 	return total / float64(len(numbers))
+// 	return total / float64(len(scores))
 // }
 
 // func main() {
-// 	fmt.Println(average(10, 5, 7))
+// 	scores := [5]float64{1,2,3,4,5}
+//   fmt.Println(average(scores))
+// }
+
+// Part 2
+// var initialPets map[string]string = map[string]string {
+// 	"frido" : "dog",
+// 	"nuffi" : "cat",
+// 	"kubus" : "horse",
+// }
+
+// func petExists(petName string) bool{
+// 	_, ok := initialPets[petName] 
+// 	return ok
+// }
+
+// func main() {
+// 	petName := "frido"
+// 	fmt.Println(petExists(petName))
+// }
+
+// Part 3
+// var groceries = []string{"apple", "lemon", "eggs"}
+// func totalGroceries(additionalGroceries ...string) []string{
+// 	foods := groceries
+// 	for _, g := range additionalGroceries {
+// 		foods = append(groceries, g)
+// 	}
+// 	return foods
+// }
+// func main() {
+// 	fmt.Println(totalGroceries("milk"))
 // }
