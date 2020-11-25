7592b44c5f8d6c14feb9f74caa5beac051010394
Katarzyna <kolnykatarzyna@gmail.com>
2020-11-25 18:09:31 +0000

    Feat: solution 7 POINTERS

--- 07_pointers/code/exercise_7a_solution.go
+++ 07_pointers/code/exercise_7a_solution.go
@@ -1,27 +1,25 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// // A User is a user type
-// type User struct {
-// 	ID                         int
-// 	FirstName, LastName, Email string
-// }
+type User struct {
+	ID                         int
+	FirstName, LastName, Email string
+}
 
-// var u = User{
-// 	ID:        1,
-// 	FirstName: "Marilyn",
-// 	LastName:  "Monroe",
-// 	Email:     "marilynmonroe@gmail.com",
-// }
+// YOUR CODE HERE
+var u = User {
+	ID: 1,
+	FirstName: "Kasia",
+	LastName: "Kolny",
+	Email: "kolnykatarzyna@gmail.com",
+}
+func updateEmail(u *User) { //passing address
+	u.Email = "kasia@gmail.com" // no need for * since struct
+	fmt.Println("updated Email:", u.Email)
+}
+func main() {
+		fmt.Println("Pointers!")
+		updateEmail(&u) //invoking passing address
+}
 
-// func updateEmail(u *User) {
-// 	u.Email = "newEmail@gmail.com"
-// 	fmt.Println("in update email: ", u.Email)
-// }
-
-// func main() {
-// 	fmt.Println("Pointers!")
-// 	updateEmail(&u)
-// 	fmt.Println("Updated User: ", u)
-// }
